b0880586f4c064e0f4aa9e735917fda45e18f1f3
seoyeonjiin <duswls214@khu.ac.kr>
2024-09-15 00:19:42 +0900

    Feat: add s3 presigned download url logic to search service

--- src/search/search.module.ts
+++ src/search/search.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { SearchService } from './search.service';
 import { SearchController } from './search.controller';
 import { HttpModule } from '@nestjs/axios';
+import { S3Service } from 'src/s3/s3.service';
 
 @Module({
   imports: [HttpModule],
   controllers: [SearchController],
-  providers: [SearchService],
+  providers: [SearchService, S3Service],
 })
 export class SearchModule {}
--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -1,10 +1,14 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { lastValueFrom } from 'rxjs';
+import { S3Service } from 'src/s3/s3.service';
 
 @Injectable()
 export class SearchService {
-  constructor(private readonly httpService: HttpService) {}
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly s3Service: S3Service,
+  ) {}
   async searchCakes(keyword: string, page: string) {
     const pageInt = parseInt(page, 10);
     const size = 10;
@@ -13,6 +17,13 @@ export class SearchService {
 
     const searchResponse = await lastValueFrom(this.httpService.get(url));
     const searchResponseData = searchResponse.data;
+
+    for (const cake of searchResponseData.result) {
+      cake.photo = await this.s3Service.generagePresignedDownloadUrl(
+        cake.photo,
+      );
+    }
+
     return searchResponseData;
   }
 }
